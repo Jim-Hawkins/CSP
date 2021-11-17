2e986791f413e8d52c7ec2c38f8679b61754cde2
Jim Hawkins <sergiogilnova2001@gmail.com>
2021-11-17 12:32:32 +0100

    cositas

--- CSPStowage.py
+++ CSPStowage.py
@@ -1,4 +1,38 @@
 import sys
+import constraint
+
+def main(mapa, contenedores):
+
+    problem = constraint.Problem()
+    
+    problem.addVariables( [i[0] for i in contenedores], ( range(len(mapa)) ) )
+    
+    problem.addConstraint()
+    return
+
 
 if __name__ == "__main__":
-    def main(sys.argv[1], sys.argv[2], sys.argv[3])
+    if len(sys.argv) != 4:
+        print("Debe introducir: CSPStowage.py <path> <mapa> <contenedores>")
+    else:
+        mapa = []
+        with open(sys.argv[1] + "/" + sys.argv[2]) as map_file:
+            lectura = map_file.readline().split(" ")
+            lectura[-1] = lectura[-1].replace("\n", "")
+            while len(lectura) != 1:
+                mapa.append(lectura)
+                lectura = map_file.readline().split(" ")
+                lectura[-1] = lectura[-1].replace("\n", "")
+            
+        contenedores = []
+        with open(sys.argv[1] + "/" + sys.argv[3]) as container_file:
+            lectura = container_file.readline().split(" ")
+            lectura[-1] = lectura[-1].replace("\n", "")
+            while len(lectura) != 1:
+                contenedores.append(lectura)
+                lectura = container_file.readline().split(" ")
+                lectura[-1] = lectura[-1].replace("\n", "")
+
+        res = main(mapa, contenedores)
+        
+        print(res)
