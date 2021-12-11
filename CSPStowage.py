97f178ff4489c5078ea2db56cc8d6f87ea8d57d1
Jim Hawkins <sergiogilnova2001@gmail.com>
2021-12-11 22:04:18 +0100

    parte 1 casi, la gravedad sale

--- CSPStowage.py
+++ CSPStowage.py
@@ -1,89 +1,115 @@
 import sys
 import constraint
 
-def main(mapa, contenedores):
-
-    variables = list()
-    for i in contenedores:
-        variables.append(i[0])
-
-    problem = constraint.Problem()
-
-    problem.addVariables(
-                         variables,
-                         range(len(mapa) * len(mapa[0]))
-                        )
-
-    prohibidos = encuentra_celdas(contenedores, "X")
-    electrificados = encuentra_celdas(contenedores, "E")
-    '''problem.addConstraint(constraint_refrigerados, 
-                          (variables, electrificados))
-    problem.addConstraint(constraint_prohibidos, 
-                          (variables, prohibidos))
-                          '''
-    problem.addConstraint(constraint_contenedores_ordenados, 
-                          variables)
-    '''
-    problem.addConstraint(constraint_suficientes_celdas, 
-                          (mapa, contenedores, "E", "R"))
-    problem.addConstraint(constraint_suficientes_celdas, 
-                          (mapa, contenedores, "N", "S"))'''
-    return problem.getSolutions()
-
-def encuentra_celdas(contenedores, tipo):
-    res = list()
-    for i in range(len(contenedores)):
-        for j in range(len(contenedores[0])):
-            if contenedores[i][j] == tipo:
-                res.append( i*len(contenedores[0]) + j )
-    return res
-
-def constraint_refrigerados(cont, electrificados):
-    return cont in electrificados
-
-def constraint_prohibidos(cont, prohibidos):
-    return cont not in prohibidos
-
-def constraint_contenedores_ordenados(*args):
-    ''' Los contenedores que van al puerto 1 deben estar por encima
-        de los del puerto 2'''
-    print(args[0])
-    '''
-    for contA in range(len(args)):
-        for contB in range(contA+1,len(args)):
-            if contA != contB and contA!=contB:
-                print(contA, contB)
-                print("---------------")
-                print(contenedores)
-                # obtenemos el destino de cada contenedor
-                for fila in contenedores:
-                    if contenedores[contA][0] in fila:
-                        destA = fila[2]
-                    if contenedores[contB][0] in fila:
-                        destB = fila[2]
-
-            if destA == destB: return True
-            if destA == 1: return destA > destB
-    '''
-                
-
-def constraint_suficientes_celdas(mapa, contenedores, tipo_celda, tipo_cont):
-    ''' Función que determina que si hay suficientes celdas de un tipo para
-        acomodar a determinado tipo de cont'''
-    celda = 0
-    for i in range(len(mapa)):
-        for j in range(len(mapa[0])):
-            if mapa[i][j] == tipo_celda:
-                celda += 1
-    cont = 0
-    for i in range(len(contenedores)):
-        if i[1] == tipo_cont:
-            cont += 1
+class Punto:
+    def __init__(self, col, fila):
+        self.fila = fila
+        self.col = col
     
-    return celda >= cont
+    def __gt__(self, other):
+        #if self.fila == other.fila:
+        #    return self.col > self.col
+        #print(self, other, self.fila == other.fila + 1 and self.col == other.col)
+        return self.fila + 1 == other.fila and self.col == other.col
+    
+    def __str__(self):
+        return "({}, {})".format(self.col, self.fila)
+    
+    def __repr__(self):
+        return self.__str__()
 
-def constraint_gravedad():
-    pass
+class Problema:
+    def __init__(self, mapa, contenedores):
+
+        self.variables = list()
+        for i in range(len(contenedores)):
+            self.variables.append(f'{contenedores[i][0]}{contenedores[i][1]}{contenedores[i][2]}')
+
+        self.variables = tuple(self.variables)
+
+        self.dom = list()
+        for pila in range(len(mapa[0])):
+            for nivel in range(len(mapa)):
+                self.dom.append( Punto(pila, nivel) )
+
+        self.problem = constraint.Problem()
+        self.problem.addVariables(self.variables, self.dom)
+
+        #prohibidos = encuentra_celdas(contenedores, "X")
+        #electrificados = encuentra_celdas(contenedores, "E")
+
+        self.problem.addConstraint(constraint.AllDifferentConstraint(), self.variables)
+        
+        self.problem.addConstraint(self.constraint_uno_debajo_de_otro, self.variables)
+        
+        
+        '''problem.addConstraint(constraint_prohibidos, 
+                            (variables, prohibidos))
+        problem.addConstraint(constraint_contenedores_ordenados, 
+                            variables)
+        problem.addConstraint(constraint_suficientes_celdas, 
+                            (mapa, contenedores, "E", "R"))
+        problem.addConstraint(constraint_suficientes_celdas, 
+                            (mapa, contenedores, "N", "S"))'''
+        
+    def solve(self):
+        return self.problem.getSolutions()
+
+    def constraint_base(self, *args):
+        for i in range(len(args)):
+            pass
+
+    def constraint_uno_debajo_de_otro(self, *args):
+        # i = uno, j = otro
+        for i in range(len(args)):
+            condicion = []
+            for j in range(len(args)):
+                if( i != j and args[j].fila - args[i].fila == 1 and args[i].col == args[j].col or args[i].fila == 2):
+                    condicion.append(True)
+            # si para un i no se ha encontrado ningún j que satisfaga el condicional, termina
+            res = False
+            for i in condicion:
+                if i == True:
+                    res = True
+            if res == False:
+                return False
+
+        return True
+
+    def constraint_uno_encima_de_otro(self, *args):
+        # i = uno, j = otro
+        for i in range(len(args)):
+            for j in range(i+1, len(args)):
+                if args[j].fila == 2:
+                    if not ( (args[i].fila - args[j].fila == -1 and args[i].col == args[j].col) ):
+                        return False
+        return True
+    
+    def encuentra_celdas(self, contenedores, tipo):
+        res = list()
+        for i in range(len(contenedores)):
+            for j in range(len(contenedores[0])):
+                if contenedores[i][j] == tipo:
+                    res.append( (i,j) )
+        return res
+
+    def constraint_prohibidos(self, cont, prohibidos):
+        return cont not in prohibidos
+
+    def constraint_suficientes_celdas(self, mapa, contenedores, tipo_celda, tipo_cont):
+        ''' Función que determina que si hay suficientes celdas de un tipo para
+            acomodar a determinado tipo de cont'''
+        celda = 0
+        for i in range(len(mapa)):
+            for j in range(len(mapa[0])):
+                if mapa[i][j] == tipo_celda:
+                    celda += 1
+        cont = 0
+        for i in range(len(contenedores)):
+            if i[1] == tipo_cont:
+                cont += 1
+        
+        return celda >= cont
 
 
 def read_doc(path, file):
@@ -103,6 +129,7 @@ if __name__ == "__main__":
         quit()
     mapa = read_doc(sys.argv[1], sys.argv[2])        
     contenedores = read_doc(sys.argv[1], sys.argv[3])
-    res = main(mapa, contenedores)
-    
-    print(res)
+    res = Problema(mapa, contenedores).solve()
+
+    for i in res:
+        print(i)
